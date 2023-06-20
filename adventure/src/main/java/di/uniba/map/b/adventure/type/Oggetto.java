811d4c00e3d6b12476c17659de18c349ab4e1281
Michele <michele.matteucci02@gmail.com>
2023-06-20 21:09:34 +0200

    Modificate classi in Type

--- adventure/src/main/java/di/uniba/map/b/adventure/type/Oggetto.java
+++ adventure/src/main/java/di/uniba/map/b/adventure/type/Oggetto.java
@@ -13,6 +13,8 @@ import java.util.Set;
  * Classe che serve per rappresentare un oggetto nel gioco.
  */
 public class Oggetto {
+    private static final int HASH_1 = 7;
+    private static final int HASH_2 = 37;
     private final int idOggetto;
     private String nomeOggetto;
     private String descrizioneOggetto;
@@ -69,13 +71,15 @@ public class Oggetto {
 
     /**
      * Metodo get per il nome dell'oggetto.
+     * @return nome dell'oggetto.
      */
     public String getNomeOggetto() {
         return this.nomeOggetto;
     }
 
     /**
-     * Metodo set per il nome dell'oggetto.
+     * Metodo set per impostare il nome dell'oggetto.
+     * @param nome nome da impostare
      */
     public void setNome(final String nome) {
         this.nomeOggetto = nome;
@@ -83,112 +87,160 @@ public class Oggetto {
 
     /**
      * Metodo get per la descrizione dell'oggetto.
+     * @return descrizione dell'oggetto.
      */
     public String getDescrizioneOggetto() {
         return this.descrizioneOggetto;
     }
 
     /**
-     * Metodo set per la descrizione dell'oggetto.
+     * Metodo set per impostare la descrizione dell'oggetto.
+     * @param descrizione descrizione dell'oggetto.
      */
     public void setDescrizioneOggetto(final String descrizione) {
         this.descrizioneOggetto = descrizione;
     }
 
     /**
-     * Metodo booleano: Restituisce vero se un oggetto puo' essere aperto.
-     * Falso altrimenti.
+     * Metodo per verificare se un oggetto può essere aperto.
+     * @return Vero se un oggetto puo' essere aperto, Falso altrimenti
      */
     public boolean isApribile() {
         return this.apribile;
     }
 
     /**
-     * Metodo set per dare la possibilita' ad un oggetto di essere raccolto
+     * Metodo set per dare la possibilita' ad un oggetto di essere aperto.
+     * @param openable variabile booleana che indica se l'oggetto può essere aperto.
      */
     public void setApribile(final boolean openable) {
         this.apribile = openable;
     }
 
     /**
-     * Metodo booleano: Restituisce vero se un oggetto puo' essere raccolto.
-     * Falso altrimenti.
+     * Metodo per verificare se un oggetto può essere raccolto.
+     * @return Vero se un oggetto puo' essere raccolto, Falso altrimenti
      */
     public boolean isRaccogglibile() {
         return this.raccogglibile;
     }
 
-    public void setRaccogglibile(final boolean raccogglibile) {
+    /**
+     * Metodo set per dare la possibilita' ad un oggetto di essere raccolto.
+     * @param raccogglibileCorr variabile booleana che indica se l'oggetto può essere raccolto.
+     */
+    public void setRaccogglibile(final boolean raccogglibileCorr) {
         this.raccogglibile = raccogglibile;
     }
 
     /**
-     * Metodo booleano: Restituisce vero se un oggetto puo' essere premuto.
-     * Falso altrimenti.
+     * Metodo per verificare se un oggetto può essere premuto.
+     * @return Vero se un oggetto puo' essere premuto, Falso altrimenti
      */
-    public boolean IsPremibile() {
+    public boolean isPremibile() {
         return this.premibile;
     }
 
-    public void setPremibile(final boolean pushable) {
-        this.premibile = pushable;
+    /**
+     * Metodo set per dare la possibilita' ad un oggetto di essere aperto.
+     * @param premibileCorr variabile booleana che indica se l'oggetto può essere premuto.
+     */
+    public void setPremibile(final boolean premibileCorr) {
+        this.premibile = premibile;
     }
 
+    /**
+     * Metodo booleano per verificare se l'oggetto è aperto.
+     * @return Vero se l'oggetto è aperto, Falso altrimenti.
+     */
     public boolean isAperto() {
         return this.aperto;
     }
 
-    public void setAperto(final boolean open) {
-        this.aperto = open;
+    /**
+     * Metodo set per rendere un oggetto aperto.
+     * @param apertoCorr variabile booleana che indica se l'oggetto deve essere aperto o chiuso.
+     */
+    public void setAperto(final boolean apertoCorr) {
+        this.aperto = apertoCorr;
     }
 
+    /**
+     * Metodo booleano per verificare se l'oggetto è premuto.
+     * @return Vero se l'oggetto è premuto, Falso altrimenti.
+     */
     public boolean isPremuto() {
         return this.premuto;
     }
 
-    public void setPremuto(final boolean push) {
-        this.premuto = push;
+    /**
+     * Metodo set per rendere un oggetto premuto.
+     * @param premutoCorr variabile booleana che indica se l'oggetto deve essere premuto o depremuto.
+     */
+    public void setPremuto(final boolean premutoCorr) {
+        this.premuto = premutoCorr;
     }
 
+    /**
+     * Metodo che restituisce l'alias dell'oggetto.
+     * @return alias del comando.
+     */
     public Set<String> getAliasOggetto() {
         return this.aliasOggetto;
     }
-
-    public void setAlias(final Set<String> alias) {
-        this.aliasOggetto = alias;
+    /**
+     * Metodo set per impostare un insieme di alias di un comando.
+     * @param aliasOgg set di alias di comandi.
+     */
+    public void setAlias(final Set<String> aliasOgg) {
+        this.aliasOggetto = aliasOgg;
     }
-    
+    /**
+     * Metodo set per impostare un vettore alias di un oggetto.
+     * @param alias vettori di alias.
+     */
     public void setAlias(final String[] alias) {
         this.aliasOggetto = new HashSet<>(Arrays.asList(alias));
     }
 
-    public int getId() {
+    /**
+     * Metodo get che restituisce l'id dell'oggetto.
+     * @return id dell'oggetto.
+     */
+    public int getIdOggetto() {
         return this.idOggetto;
     }
 
+    /**
+     * Metodo che calcola un id univoco all'oggetto.
+     * @return id univoco.
+     */
     @Override
     public int hashCode() {
-        int hash = 7;
-        hash = 37 * hash + this.idOggetto;
+        int hash = HASH_1;
+        hash = HASH_2 * hash + this.idOggetto;
         return hash;
     }
 
+    /**
+     * @param oggetto oggetto da confrontare
+     * @return Vero se l'oggetto passato come parametro e' uguale a quello sul quale e chiamato il metodo.
+     */
     @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
+    public boolean equals(final Object oggetto) {
+        if (this == oggetto) {
             return true;
         }
-        if (obj == null) {
+        if (oggetto == null) {
             return false;
         }
-        if (getClass() != obj.getClass()) {
+        if (getClass() != oggetto.getClass()) {
             return false;
         }
-        final Oggetto other = (Oggetto) obj;
+        final Oggetto other = (Oggetto) oggetto;
         if (this.idOggetto != other.idOggetto) {
             return false;
         }
         return true;
     }
-
 }
