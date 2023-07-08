f1240fc92526ea3a77485383827bdc01529d849a
gianna <giannantoniosanrocco26@gmail.com>
2023-07-08 16:53:50 +0200

    piccola modifica

--- adventure/src/main/java/data/Classifica.java
+++ adventure/src/main/java/data/Classifica.java
@@ -23,19 +23,19 @@ public class Classifica {
     }
     
     public List<Map.Entry<String, Integer>> ottieniClassificaUtenti() {
-    Map<String, Integer> punteggioMassimoperUtente = 
-        partite.stream()
-        .filter(Partita::terminata) // filtra solo le partite con attributo "finita" a true
-        .collect(Collectors.groupingBy(Partita::getNomeUtente,
-            Collectors.mapping(Partita::getPunteggio, Collectors.maxBy(Integer::compare))))
-        .entrySet()
-        .stream()
-        .collect(Collectors.toMap(Map.Entry::getKey, e -> e.getValue().orElse(0)));
+        Map<String, Integer> punteggioMassimoperUtente = 
+            partite.stream()
+            .filter(Partita::terminata) // filtra solo le partite con attributo "finita" a true
+            .collect(Collectors.groupingBy(Partita::getNomeUtente,
+                Collectors.mapping(Partita::getPunteggio, Collectors.maxBy(Integer::compare))))
+            .entrySet()
+            .stream()
+            .collect(Collectors.toMap(Map.Entry::getKey, e -> e.getValue().orElse(0)));
 
-        //lista di coppie chiave-valore
-        List<Map.Entry<String, Integer>> entryList = new ArrayList<>(punteggioMassimoperUtente.entrySet());
-        // Ordina la lista di Map.Entry in base al valore
-        Collections.sort(entryList, Map.Entry.comparingByValue(Comparator.reverseOrder()));
-        return entryList;
+            //lista di coppie chiave-valore
+            List<Map.Entry<String, Integer>> entryList = new ArrayList<>(punteggioMassimoperUtente.entrySet());
+            // Ordina la lista di Map.Entry in base al valore
+            Collections.sort(entryList, Map.Entry.comparingByValue(Comparator.reverseOrder()));
+            return entryList;
     }
 }
