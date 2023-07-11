13cdebfa3988f5184bbbe3ab40081ff907b0802d
Michele <michele.matteucci02@gmail.com>
2023-07-11 05:12:15 +0200

    Aggiunta Javadoc package multimediali

--- adventure/src/main/java/multimediali/Immagini.java
+++ adventure/src/main/java/multimediali/Immagini.java
@@ -1,18 +1,42 @@
 package multimediali;
 
 import javax.imageio.ImageIO;
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.BorderFactory;
+import java.awt.Image;
+import java.awt.FlowLayout;
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-public class Immagini {
+/**
+ * Classe di utilità per la gestione delle immagini.
+ * Questa classe fornisce metodi per caricare e manipolare immagini.
+ * Il costruttore è dichiarato privato per evitare l'istanziazione di oggetti Immagini.
+ */
+public final class Immagini {
 
+    /**
+     * Classe di utilità per la gestione delle immagini.
+     * Questa classe fornisce metodi per caricare e manipolare immagini.
+     * Il costruttore è dichiarato privato per evitare l'istanziazione di oggetti Immagini.
+     */
     private Immagini() {
 
     }
 
+    /**
+     * Carica un'immagine specificata dal percorso e nome dell'immagine e la visualizza come sfondo in un JPanel.
+     *
+     * @param percorso      Percorso dell'immagine.
+     * @param nomeImmagine  Nome dell'immagine.
+     * @param panel         JPanel in cui visualizzare l'immagine come sfondo.
+     * @param background    JLabel utilizzato come sfondo per l'immagine.
+     */
     public static void caricaImmagine(final String percorso, final String nomeImmagine, final JPanel panel, final JLabel background) {
         try {
             BufferedImage img = ImageIO.read(new File((percorso + nomeImmagine + ".png")));
@@ -25,26 +49,39 @@ public class Immagini {
             panel.revalidate();
             panel.repaint();
         } catch (IOException e) {
-            System.out.println("Errore nell'aggiunta dell'immagine");
+            System.out.println("Errore nell'aggiunta dell'immagine: " + e.getMessage());
         }
     }
-    
+
+    /**
+     * Carica un'immagine specificata dal percorso e nome dell'immagine e la visualizza come sfondo in un JPanel.
+     * L'immagine viene adattata alle dimensioni del pannello in modo da mantenere l'aspetto originale.
+     * Il pannello viene reso trasparente e senza bordi.
+     *
+     * @param percorso      Percorso dell'immagine.
+     * @param nomeImmagine  Nome dell'immagine.
+     * @param panel         JPanel in cui visualizzare l'immagine come sfondo.
+     */
     public static void caricaImmagine(final String percorso, final String nomeImmagine, final JPanel panel) {
         try {
             BufferedImage img = ImageIO.read(new File((percorso + nomeImmagine + ".png")));
             Image dimg = img.getScaledInstance(panel.getWidth(), panel.getHeight(), Image.SCALE_SMOOTH);
             ImageIcon imageIcon = new ImageIcon(dimg);
-            panel.setBackground(new Color(0,0,0,0));
+
+            panel.setBackground(new Color(0, 0, 0, 0));
             panel.setOpaque(false);
             panel.setBorder(BorderFactory.createEmptyBorder());
             panel.setLayout(new BorderLayout());
+
             JLabel background = new JLabel(imageIcon);
             panel.add(background);
             background.setLayout(new FlowLayout());
+
             panel.revalidate();
             panel.repaint();
         } catch (IOException e) {
-            System.out.println("Errore nell'aggiunta dell'immagine");
+            System.out.println("Errore nell'aggiunta dell'immagine: " + e.getMessage());
         }
     }
+
 }
--- adventure/src/main/java/multimediali/Suono.java
+++ adventure/src/main/java/multimediali/Suono.java
@@ -1,6 +1,5 @@
 package multimediali;
 
-
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
@@ -13,9 +12,10 @@ import java.io.IOException;
 import java.util.Optional;
 
 /**
- *
+ * Classe di utilità per la gestione del suono.
+ * Questa classe fornisce metodi per la riproduzione e l'interruzione del suono.
  */
-public class Suono {
+public final class Suono {
     private static volatile boolean suonoAttivo = false;
     private static Optional<SourceDataLine> lineOptional = Optional.empty();
     //private static ReentrantLock lock = new ReentrantLock();
@@ -23,22 +23,31 @@ public class Suono {
     private static final int BYTETRACCIA = 4096;
 
     /**
+     * Classe di utilità per la gestione del suono.
+     * Questa classe fornisce metodi per la riproduzione e l'interruzione del suono.
+     */
+    private Suono() {
+        // Il costruttore è dichiarato privato per evitare l'istanziazione di oggetti Suono.
+    }
+
+    /**
+     * Riproduce una traccia audio specificata dal percorso relativo.
      *
-     * @param percorsoRel
-     * @param loop
+     * @param percorsoRel Percorso relativo della traccia audio.
+     * @param loop        Flag che indica se la traccia audio deve essere riprodotta in loop.
      */
     public static void riproduciTraccia(final String percorsoRel, final boolean loop) {
         if (suonoAttivo) {
             stopRiproduzione();
         }
-
-        //lock.lock();
+        // Crea un nuovo thread per la riproduzione audio
         Thread audioThread = new Thread(() -> {
             try {
                 String estensione = ".wav";
                 String percorso = percorsoRel + estensione;
                 System.out.println(percorso);
                 File audioFile = new File(percorso);
+                // Ottiene l'input stream audio dalla traccia audio
                 AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(audioFile);
                 AudioFormat audioFormat = audioInputStream.getFormat();
                 DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioFormat);
@@ -52,6 +61,7 @@ public class Suono {
                 byte[] buffer = new byte[BYTETRACCIA];
                 int bytesRead;
 
+                // Riproduce la traccia audio finché il flag di loop è true e il suono è attivo
                 do {
                     while ((bytesRead = audioInputStream.read(buffer)) != -1) {
                         line.write(buffer, 0, bytesRead);
@@ -64,25 +74,34 @@ public class Suono {
             } catch (IOException | LineUnavailableException | UnsupportedAudioFileException e) {
                 System.err.println(e.getMessage());
             } finally {
+                // Chiude la linea di riproduzione audio e imposta il flag di suono attivo a false
                 if (lineOptional.isPresent()) {
                     SourceDataLine line = lineOptional.get();
                     line.stop();
                     line.close();
                     lineOptional = Optional.empty();
                     suonoAttivo = false;
-                    //lock.unlock();
                 }
             }
         });
+        // Avvia il thread di riproduzione audio
         audioThread.start();
         thread = audioThread;
+        /*
+        Il metodo riproduciTraccia permette di riprodurre una traccia audio specificata dal percorso relativo.
+        La traccia audio viene riprodotta utilizzando un nuovo thread per non bloccare l'esecuzione principale del programma.
+        Viene creato un oggetto AudioInputStream per ottenere l'input stream audio dalla traccia audio.
+        Successivamente, viene creato un oggetto SourceDataLine per gestire la riproduzione audio.
+        La traccia audio viene letta e scritta nella linea di riproduzione finché il flag di loop è true e il suono è attivo.
+        Una volta terminata la riproduzione, la linea di riproduzione viene chiusa e il flag di suono attivo viene impostato a false.
+         */
     }
 
     /**
-     *
+     * Interrompe la riproduzione audio in corso e chiude la linea di riproduzione.
      */
     public static void stopRiproduzione() {
-        //lock.lock();
+        // Interrompe il thread di riproduzione audio
         thread.interrupt();
         if (suonoAttivo && lineOptional.isPresent()) {
             SourceDataLine line = lineOptional.get();
@@ -92,4 +111,5 @@ public class Suono {
             suonoAttivo = false;
         }
     }
+
 }
