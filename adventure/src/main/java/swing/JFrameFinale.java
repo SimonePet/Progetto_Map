46c285a52f8cbf8c0d19db126590485a70fee9a2
gianna <giannantoniosanrocco26@gmail.com>
2023-07-12 17:05:39 +0200

    risolto bug

--- adventure/src/main/java/swing/JFrameFinale.java
+++ adventure/src/main/java/swing/JFrameFinale.java
@@ -7,6 +7,7 @@ package swing;
 import di.uniba.map.b.adventure.Utils;
 import java.awt.Color;
 import javax.swing.ImageIcon;
+import javax.swing.ScrollPaneConstants;
 import multimediali.Immagini;
 import multimediali.Suono;
 
@@ -48,11 +49,10 @@ public class JFrameFinale extends javax.swing.JFrame {
         });
 
         lblTitolo.setFont(new java.awt.Font("Segoe UI", 1, 16)); // NOI18N
-        lblTitolo.setText("OH NO! UNO SQUALO ROVESCIA L'IMBARCAZIONE E TI BUTTA IN MARE");
+        lblTitolo.setText("SQUALO");
 
         textArea.setColumns(20);
         textArea.setRows(5);
-        textArea.setText("Forse sarebbe stato meglio rimanere nell'isola...\nOra sei 1 vs 1 contro uno squalo.\nBuona fortuna!");
         scrollPanel.setViewportView(textArea);
 
         javax.swing.GroupLayout jPanelLayout = new javax.swing.GroupLayout(jPanel);
@@ -60,13 +60,11 @@ public class JFrameFinale extends javax.swing.JFrame {
         jPanelLayout.setHorizontalGroup(
             jPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanelLayout.createSequentialGroup()
-                .addGap(129, 129, 129)
-                .addComponent(scrollPanel, javax.swing.GroupLayout.PREFERRED_SIZE, 345, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanelLayout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(lblTitolo, javax.swing.GroupLayout.PREFERRED_SIZE, 617, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+                .addGap(74, 74, 74)
+                .addGroup(jPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(lblTitolo, javax.swing.GroupLayout.PREFERRED_SIZE, 173, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(scrollPanel, javax.swing.GroupLayout.PREFERRED_SIZE, 497, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(74, Short.MAX_VALUE))
         );
         jPanelLayout.setVerticalGroup(
             jPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -74,8 +72,8 @@ public class JFrameFinale extends javax.swing.JFrame {
                 .addGap(25, 25, 25)
                 .addComponent(lblTitolo, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addComponent(scrollPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(127, Short.MAX_VALUE))
+                .addComponent(scrollPanel, javax.swing.GroupLayout.PREFERRED_SIZE, 170, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(53, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -94,6 +92,10 @@ public class JFrameFinale extends javax.swing.JFrame {
 
     private void formWindowOpened(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowOpened
         Color c = new Color(255, 255, 255, 150);
+        scrollPanel.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollPanel.setOpaque(false);
+        scrollPanel.getViewport().setOpaque(false);
+        scrollPanel.setBackground(c);
         jPanel.setOpaque(false);
         jPanel.setBackground(c);
         textArea.setEditable(false);
@@ -103,7 +105,7 @@ public class JFrameFinale extends javax.swing.JFrame {
         textArea.setText("Lentamente riapri gli occhi e ti ritrovi in un luogo strano e "
                 + "sconosciuto, percepisci il mare agitato\n"
                 + "Ricordi solo di essere scappato con una zattera da un'isola deserta, l'acqua che invadeva la tua barca e il vento che urlava intorno a te.\n"
-                + "Ad un tratto capisci di essere finito nella grande bocca di una balena...\n"
+                + "Ad un tratto capisci di essere finito nella grande bocca di uno squalo...\n"
                 + "Ti guardi intorno e incontri tuo nonno Geppetto!!!\n"
                 + "Rimboccati le mani la tua avventura ha inizio!");
         ImageIcon icon = new ImageIcon(Utils.PERCORSO_IMMAGINI_ICONE + "iconaGioco.png");
